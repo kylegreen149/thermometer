dfe3c8e135d3e70ac3bb26cf620edbc07f55ab17
Kyle Greenidge <kylegreen149@gmail.com>
2025-01-10 18:55:10 -0500

    Creates basic function to interpret conversion type

---
+++ main.py
@@ -0,0 +1,4 @@
+def choose_conversion_type():
+    prompt = int(input("""Choose a prompt by pressing 1 or 2: 
+                   1) Fahrenheit to Celsius
+                   2) Celsius to Fahrenheit"""))
