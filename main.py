b464f924e6d231696874bfbc5bb9a060a3741358
Kyle Greenidge <kylegreen149@gmail.com>
2025-01-10 19:09:44 -0500

    Creates while loop to ask question when an invalid answer is entered; inInitialize f_to_c and c_to_f functions

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 def choose_conversion_type():
-    prompt = int(input("""Choose a prompt by pressing 1 or 2: 
-                   1) Fahrenheit to Celsius
-                   2) Celsius to Fahrenheit"""))
+    while True:
+        prompt = int(input("""Choose a prompt by pressing 1 or 2: 
+        1) Fahrenheit to Celsius
+        2) Celsius to Fahrenheit
+        """))
+        if prompt == 1:
+            f_to_c()
+            break
+        elif prompt == 2:
+            c_to_f()
+            break
+        else:
+            print("Only enter the numbers '1' or '2'")      
+
+def f_to_c():
+    pass
+
+def c_to_f():
+    pass
+
+choose_conversion_type()
