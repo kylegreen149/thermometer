5b737cb5374157a7217093a92b0aa6ed3f2a0d12
Kyle Greenidge <kylegreen149@gmail.com>
2025-01-12 16:49:37 -0500

    Creates handleConversion function to handle conversion type, helps differentiate conversion type based on selection from drop down menu

--- thermometer/src/App.js
+++ thermometer/src/App.js
@@ -5,6 +5,19 @@ function App() {
   const [conversionType, setConversionType] = useState("FtoC")
   const [temperature, setTemperature] = useState("")
   const [result, setResult] = useState(null)
+
+  function handleConversion(e) {
+    e.preventDefault()
+    let convertedTemp;
+    if (conversionType === "FtoC") {
+      convertedTemp = ((temperature - 32) * 5 / 9).toFixed(0)
+    }
+    else if (conversionType === "CtoF") {
+      convertedTemp = ((temperature - 32) * 5 / 9).toFixed(0)
+    }
+    setResult(convertedTemp)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
